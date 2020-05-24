59de40d106f1c324c5c4f9d1adb7fa1f84bd97fc
野口智矢 <noguchitomoya@noguchisatoshiyanoMacBook-Pro.local>
2020-05-25 00:05:21 +0900

    [Add] books edit create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'book#index'
+  devise_for :users
+  
+  resources :books, only: [:new, :create, :index, :show,:edit,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :index, :show,:edit,:update]
 end
